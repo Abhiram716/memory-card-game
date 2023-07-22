7923d3003803ecf4df988f18291c54488b4a5303
Abhiram Kantipudi <a4abhiram125@gmail.com>
2023-07-22 19:03:54 +0530

    change: moved fetchData() func to useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useState,useEffect } from 'react';
-import './App.css';
-import Board from './Components/Board';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+import Board from "./Components/Board";
 
 function App() {
   const [cardsData, setCardsData] = useState([]);
@@ -9,16 +9,20 @@ function App() {
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
 
-  const fetchData = async() => {
-    fetch(`https://rickandmortyapi.com/api/character?page=${Math.floor(Math.random() * 41)}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const shuffledArray = shuffleArray(data.results);
-        setCardsData(shuffledArray);
-    });
-  }
-
   useEffect(() => {
+    const fetchData = async () => {
+      fetch(
+        `https://rickandmortyapi.com/api/character?page=${Math.floor(
+          Math.random() * 41
+        )}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const shuffledArray = shuffleArray(data.results);
+          setCardsData(shuffledArray);
+        });
+    };
+
     fetchData();
   }, []);
 
@@ -26,10 +30,9 @@ function App() {
     if (clickedCardIds.includes(cardId)) {
       setCurrentScore(0);
       setClickedCardIds([]);
-    } 
-    else {
+    } else {
       setCurrentScore(currentScore + 1);
-      
+
       if (currentScore + 1 > bestScore) {
         setBestScore(currentScore + 1);
       }
@@ -42,8 +45,8 @@ function App() {
   }
 
   function shuffleArray(array) {
-    for(let i = array.length - 1; i>0; i--){
-      const j = Math.floor(Math.random()*(i+1));
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
       const temp = array[i];
       array[i] = array[j];
       array[j] = temp;
@@ -55,12 +58,10 @@ function App() {
     <div className="app">
       <header className="App-header"></header>
       <div className="App-body">
-        <Board data={cardsData} onCardClick={onCardClick}/>
+        <Board data={cardsData} onCardClick={onCardClick} />
       </div>
     </div>
   );
 }
 
 export default App;
-
-
